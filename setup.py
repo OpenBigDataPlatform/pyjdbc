1ddce381c30e52739b4a18fe78a37001950561a0
baurelius <baurelius@protonmail.com>
2020-12-21 19:52:35 -0700

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyjdbc',
-    version='0.1.0',
+    version='0.1.1',
     author='OpenBigDataProject',
     license='Apache-2.0',
     url='https://github.com/OpenBigDataPlatform/pyjdbc',
